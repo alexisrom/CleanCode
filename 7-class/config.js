ca8b9fab3e7cc320e40194805d995c572e437d41
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-12-12 11:04:26 +0100

    6 clone 7

---
+++ 7-class/config.js
@@ -0,0 +1,26 @@
+export class Config {
+  constructor() {
+    this.COLUMNS = 140;
+    this.ROWS = 70;
+    this.IS_DEAD = 0;
+    this.IS_ALIVE = 1;
+    this.REPRODUCTION = 3;
+    this.UNDER_POPULATION = 2;
+    this.OVER_POPULATION = 3;
+  }
+}
+export class CanvasConfig {
+  constructor() {
+    this.DEAD_COLOR = "#eee";
+    this.ALIVE_COLOR = "#0080ff";
+    this.CELL_SQUARE_PXS = 10;
+  }
+}
+export class TestConfig {
+  constructor() {
+    this.DELAY_MS = 50;
+    this.INITIALIZATION_TIME = Date.now();
+    this.TIMING_TEST_MS = 5000;
+    this.generations = 0;
+  }
+}
