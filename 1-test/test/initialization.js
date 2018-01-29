15b17996f662f3d23a2b5f5fe834ccc875868f9b
Alberto Basalo <albertobasalo@agorabinaria.com>
2018-01-29 12:57:09 +0100

    test

--- 1-test/test/initialization.js
+++ 1-test/test/initialization.js
@@ -6,53 +6,53 @@ export function testInitialization(game) {
 }
 function testGrid(game, grid, name) {
   console.group(`describe ${name} grid`);
-  testSizeGrid(game, grid);
-  testContentGrid(grid);
+  testSizeGrid();
+  testContentGrid();
   console.groupEnd();
-}
-function testSizeGrid(game, grid) {
-  console.group("it should have a correct size");
-  checkExpects();
-  console.groupEnd();
-  function checkExpects() {
-    console.assert(
-      hasBegin(grid),
-      `should has a begin cell but has no begin`,
-      game.grid
-    );
-    console.assert(
-      hasEnd(grid, game.gridWidth, game.gridHeight),
-      `has no end`,
-      game.grid
-    );
-    console.assert(
-      isNotOversized(grid, game.gridWidth, game.gridHeight),
-      `is oversized`,
-      game.grid
-    );
+  function testSizeGrid() {
+    console.group("it should have a correct size");
+    checkExpects();
+    console.groupEnd();
+    function checkExpects() {
+      console.assert(
+        hasBegin(grid),
+        `should has a begin cell but has no begin`,
+        game.grid
+      );
+      console.assert(
+        hasEnd(grid, game.gridWidth, game.gridHeight),
+        `has no end`,
+        game.grid
+      );
+      console.assert(
+        isNotOversized(grid, game.gridWidth, game.gridHeight),
+        `is oversized`,
+        game.grid
+      );
+    }
+    function hasBegin(grid) {
+      return grid[0][0] !== null;
+    }
+    function hasEnd(grid, gridWidth, gridHeight) {
+      return grid[gridWidth - 1][gridHeight - 1] !== null;
+    }
+    function isNotOversized(grid, gridWidth, gridHeight) {
+      return (
+        grid[gridWidth] == undefined &&
+        grid[0][gridHeight] == undefined
+      );
+    }
   }
-  function hasBegin(grid) {
-    return grid[0][0] !== null;
-  }
-  function hasEnd(grid, gridWidth, gridHeight) {
-    return grid[gridWidth - 1][gridHeight - 1] !== null;
-  }
-  function isNotOversized(grid, gridWidth, gridHeight) {
-    return (
-      grid[gridWidth] == undefined &&
-      grid[0][gridHeight] == undefined
-    );
-  }
-}
-function testContentGrid(grid) {
-  console.group("it should have a correct content");
-  grid.forEach(column => {
-    column.forEach(row => {
-      console.assert(isValueOK(row), `has invalid data`, row);
+  function testContentGrid() {
+    console.group("it should have a correct content");
+    grid.forEach(column => {
+      column.forEach(row => {
+        console.assert(isValueOK(row), `has invalid data`, row);
+      });
     });
-  });
-  console.groupEnd();
-  function isValueOK(value) {
-    return Array.isArray(value) || value === 1 || value === 0;
+    console.groupEnd();
+    function isValueOK(value) {
+      return Array.isArray(value) || value === 1 || value === 0;
+    }
   }
 }
